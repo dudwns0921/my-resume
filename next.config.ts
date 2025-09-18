d9f52bd78af2d6dc86364bb83837c3cc93572779
dudwns0921 <dudwns0921@gmail.com>
2025-09-18 23:16:42 +0900

    fix: nextjs assetPrefix 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  assetPrefix: '/my-resume-next/',
 };
 
 export default nextConfig;
